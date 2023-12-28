e7450258d96084d2748c8f5fd2909ca35486d24c
Erik Hanchett <ehhanche@amazon.com>
2023-12-28 10:34:21 -0800

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import { Authenticator } from "@aws-amplify/ui-vue";
+import { signInWithRedirect, signOut } from "aws-amplify/auth";
+
+const onClick = () => {
+  signInWithRedirect();
+};
+</script>
+
+<template>
+  <main>
+    <h1>Hello World From Auth</h1>
+    <!-- <Authenticator :social-providers="['google']">
+      <template #="{ signOut }">
+        <button @click="signOut">Sign out</button>
+      </template>
+    </Authenticator> -->
+    <button @click="onClick">Click me for cognito</button>
+    <button @click="signOut()">Sign Out</button>
+  </main>
+</template>
